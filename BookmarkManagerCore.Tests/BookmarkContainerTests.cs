81d1ea426f178487436fdae748498a662480369f
Marcus Pallinger <git@marcus.pallinger.org>
2022-06-19 17:35:38 +1000

    Create a bookmark container

---
+++ BookmarkManagerCore.Tests/BookmarkContainerTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace BookmarkManagerCore.Tests;
+
+public class BookmarkContainerTests
+{
+    // Get a new bookmark container
+    [Fact()]
+    public void GetGetBookmarkContainer()
+    {
+        var bookmarkContainer = BookmarkContainer.GetBookmarkContainer();
+        Assert.NotNull(bookmarkContainer);
+        _ = Assert.IsType<BookmarkContainer>(bookmarkContainer);
+    }
+}
---
+++ BookmarkManagerCore/BookmarkContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace BookmarkManagerCore;
+public class BookmarkContainer
+{
+    public static BookmarkContainer GetBookmarkContainer()
+    {
+        return new BookmarkContainer();
+    }
+}
